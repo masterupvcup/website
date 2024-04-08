ce0fa4c1221c6abbe5799322db573317ce358c6e
masterupvcup <166452004+masterupvcup@users.noreply.github.com>
2024-04-09 00:12:43 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
       ]
     },
-    baseURL: '/nuxt3-ghpages/'
+    baseURL: ''
   },
   modules: [
   ],
